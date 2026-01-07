90f1cb1e177e4679cedebef22744e59c855b1eb4
Sebastian Vasquez <sebitas0489@gmail.com>
2026-01-07 18:59:31 -0500

    Corrección: se habilita modal de confirmación para eliminación de usuarios

--- view/GestionUsuarios.php
+++ view/GestionUsuarios.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once '../model/user.php';
 $user = new User();
 $usuarios = $user->getUser();
@@ -6,22 +6,24 @@ $usuarios = $user->getUser();
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gestión de Personal</title>
   <link rel="stylesheet" href="../css/inventory.css">
-  <link rel="stylesheet" href="../../css/modal.css"/>
+  <link rel="stylesheet" href="../css/modal.css" />
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs/build/css/alertify.min.css" />
   <script src="https://cdn.jsdelivr.net/npm/alertifyjs/build/alertify.min.js"></script>
 
 </head>
+
 <body>
   <div class="container mt-5 position-relative">
-   <?php include '../view/bases/base4.php'; ?>
-    
+    <?php include '../view/bases/base4.php'; ?>
+
     <header class="d-flex justify-content-between align-items-center mb-4 flex-wrap">
       <div class="d-flex align-items-center mb-3 mb-md-0">
         <img src="https://cdn.glitch.global/05dd2f16-2c70-4bf2-a8e5-35c1a876912e/logo.png?v=1740605968751" alt="Logo" style="height: 40px; margin-right: 5px;">
@@ -31,7 +33,7 @@ $usuarios = $user->getUser();
           <a href="AgregarUsuario.php" class="btn btn-outline-secondary mx-0.5 mb-0.5">Agregar Empleado</a>
         </div>
       </div>
-    
+
     </header>
 
     <div class="search-bar">
@@ -67,11 +69,11 @@ $usuarios = $user->getUser();
                 <a href="EditarUsuario.php?action=editar&id=<?= $usuario['id'] ?>" title="Editar">
                   <i class="fa-solid fa-user-pen"></i>
                 </a>
-               <a href="#" class="eliminar-btn" 
-                data-id="<?= $usuario['id'] ?>" 
-                data-nombre="<?= $usuario['nombre_usuario'] ?>" title="Eliminar">
-                 <i class="fa fa-trash"></i>
-                 </a>
+                <a href="#" class="eliminar-btn"
+                  data-id="<?= $usuario['id'] ?>"
+                  data-nombre="<?= $usuario['nombre_usuario'] ?>" title="Eliminar">
+                  <i class="fa fa-trash"></i>
+                </a>
               </td>
             </tr>
           <?php endforeach; ?>
@@ -81,140 +83,34 @@ $usuarios = $user->getUser();
 
     <div id="result-container" class="alert alert-info text-center" style="display: none;"></div>
   </div>
-  
-  
-</body>
-</html>
-
-
-
-
- 
-
- <script src="../../modal.js"></script>
-  <?php include '../view/bases/base2.php'; ?>
-  <?php include '../view/bases/base1.php'; ?>
-
- 
 
 
 </body>
+
 </html>
 
+<!-- MODAL CONFIRMACIÓN ELIMINAR -->
+<div id="modal-confirm" class="modal-overlay">
+  <div class="modal-content">
+    <h5>Confirmar eliminación</h5>
+    <p>¿Está seguro de eliminar este usuario?</p>
+    <div class="text-right">
+      <a id="btn-confirm" class="btn btn-danger">Eliminar</a>
+      <button id="btn-cancel" class="btn btn-secondary">Cancelar</button>
+    </div>
+  </div>
+</div>
 
 
 
 
+<script src="../model/js/modal.js"></script>
+<?php include '../view/bases/base2.php'; ?>
+<?php include '../view/bases/base1.php'; ?>
 
 
 
 
+</body>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</html>
